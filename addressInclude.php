8a1e7a6d2dbde504f9278aaa51be63f301a92112
mlovell82 <mlovell@kent.edu>
2017-01-30 12:57:14 -0500

    Create addressInclude.php

---
+++ addressInclude.php
@@ -0,0 +1,18 @@
+// addressInclude.php
+
+<?php
+
+function doDB() {
+	global $mysqli;
+	
+	//connect to server and select database; you may need it
+	$mysqli = mysqli_connect("localhost", "mick", "the3pigs", "address_book");
+	
+	
+	//if connection fails, stop script execution
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+}
+?>
